a15253eb2c48bd98efcb965c6d8b37fbcfcb89f2
Alexander Eberspächer <alex.eberspaecher@googlemail.com>
2012-10-01 21:44:01 +0200

    Add continuous k implementation.
    Switch all formulas to those taken from D. Tannor's excellent book on
    quantum mechanics. Those are easier to read. Also make use of OpenMP
    paralellization of an inner loop (although this certainly won't be the
    bottleneck in the FGH method).

--- FGH.py
+++ FGH.py
@@ -5,34 +5,32 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from Hamiltonian import h as H  # TODO: fix lowercase name
+from Hamiltonian import h as H, hcont as HCont  # TODO: fix lowercase name
 from scipy.linalg import eigh
 
-L = 10  # length of x-interval to use
-N = 2**11  # number of samples
+L = 50  # length of x-interval to use
+N = 2**12  # number of samples
 NplotMin = 0  # quantum number of first eigenfunction to plot
-Nplot = 3  # number of eigenfunctions to plot
+Nplot = 5  # number of eigenfunctions to plot
 
 # define a potential:
-pot = lambda x: 0.5 * 3.0**2 * (x-L/2.)**2 # harmonic oscillator potential, centered
+pot = lambda x: 0.5 * 3.0**2 * (x-L/2.)**2  # harmonic oscillator potential, centered
 
 # define a grid:
 x = np.linspace(0, L, N)
 
 # sample potential:
 Vsampled = pot(x)
-Hsampled = H(L, Vsampled)
+Hsampled = HCont(L, Vsampled)
 
-# diagonalize the Hamiltonian
+# diagonalize the Hamiltonian matrix:
 E, psi = eigh(Hsampled)
 
-#plt.plot(x, Vsampled, c="k", ls="-", lw=2)
 for i in range(NplotMin, NplotMin+Nplot):
     plt.plot(x, np.abs(psi[:,i])**2, ls="--", lw=2)
     print("E[%s] = %s"%(i, E[i]))
 
-#plt.plot(x, np.abs((3/np.pi)**0.25 * np.exp(-3*((x-L/2.)**2)/2))**2)  # groundstate WF for omega=3
 plt.show()
 
-# TODO: plot potential and WF
+# TODO: plot potential *and* WF
 # TODO: perform physical normalization of the WFs
--- Hamiltonian.f90
+++ Hamiltonian.f90
@@ -6,11 +6,15 @@
 ! cmp. BALINT-KURTI et al., INTERNATIONAL REVIEWS IN PHYSICAL CHEMISTRY, VOL. 11, No. 2, 317-344
 ! (http://www.tandfonline.com/toc/trpc20/11/2)
 
+! The implementations actually used are taken from D. Tannor's book
+! "Introdoction to Quantum Mechanics: a time-dependent perspective"
+
 ! compile to a Python module with
-! f2py --f90flags="-ftree-vectorize -ffree-line-length-none -fmax-errors=2 -Wall -Wextra -Warray-temporaries" -m Hamiltonian -c Hamiltonian.f90
+! f2py --f90flags="-fopenmp -ftree-vectorize -ffree-line-length-none -fmax-errors=2 -Wall -Wextra -Warray-temporaries" -lgomp -m Hamiltonian -c Hamiltonian.f90
 
 subroutine H(N, L, V, Hij)
   ! compute the Hamiltonian NxN matrix in atomic units (hbar = 1; m = 1)
+  ! according to Tannor's book, sec. 11.6.4
   implicit none
   integer, intent(in) :: N  ! number of samples for the potential, also: output dimension dim([H_ij]) = N
   double precision, intent(in) :: L  ! length of interval
@@ -18,23 +22,55 @@ subroutine H(N, L, V, Hij)
   double precision, dimension(N,N), intent(out) :: Hij
 
   integer :: i, j
-  double precision :: diagConst, Nreal
+  double precision :: K  ! wavenumbers are in [-K, +K]
   double precision, parameter :: pi = 4.0d0*atan(1.0d0)
 
-  ! note that we chose hbar to be 1, not h. thus, we have a few h = 2pi factors
-
-  Nreal = real(N, kind=kind(1.0d0))
-  diagConst = (2*pi)**2/(4.0d0*L**2)*( (Nreal-1.0d0)*(Nreal-2.0d0)/6.0d0 + Nreal/2.0d0 )  ! note the N/2 instead of the 1 in the reference
+  K = pi/(L/N)
 
   do i=1,N
+     !$OMP PARALLEL DO PRIVATE(j) SHARED(Hij, i, K, N)
      do j=1,i
         if(i == j) then
-           Hij(i, j) = diagConst + V(i)
+           Hij(i, j) = 0.5d0*(K**2/3.0d0*(1.0d0 + 2.0d0/N**2)) + V(i)
         else
-           Hij(i, j) = (-1)**(i-j) * (2*pi/(2.0d0*L*sin(pi*(i-j)/N)))**2
-           Hij(j,i) = Hij(i,j)  ! use Hermitian symmetry
+           Hij(i, j) = K**2/N**2 * (-1)**(j-i) / (sin(pi*(j-i)/N))**2
+           Hij(j, i) = Hij(i, j)  ! use Hermitian symmetry
         end if
      end do
+     !$OMP END PARALLEL DO
   end do
 
 end subroutine H
+
+subroutine HCont(N, L, V, Hij)
+  ! compute the Hamiltonian NxN matrix in atomic units (hbar = 1; m = 1)
+  ! according to Tannor's book, sec. 11.6.4
+  ! This routine uses a continuous k variable - thus, no assumptions on periodic
+  ! boundary conditions are made
+  implicit none
+  integer, intent(in) :: N  ! number of samples for the potential, also: output dimension dim([H_ij]) = N
+  double precision, intent(in) :: L  ! length of interval
+  double precision, dimension(N), intent(in) :: V  ! sampled potential V(x_i)
+  double precision, dimension(N,N), intent(out) :: Hij
+
+  integer :: i, j
+  double precision :: K  ! wavenumbers are in [-K, +K]
+  double precision, parameter :: pi = 4.0d0*atan(1.0d0)
+
+  K = pi/(L/N)
+
+  do i=1,N
+     !$OMP PARALLEL DO PRIVATE(j) SHARED(Hij, i, K, N)
+     do j=1,i
+        if(i == j) then
+           Hij(i, j) = 0.5d0*(K**2/3.0d0) + V(i)
+        else
+           Hij(i, j) = K**2/pi**2 * (-1)**(j-i) / (j-i)**2
+           Hij(j, i) = Hij(i, j)  ! use Hermitian symmetry
+        end if
+     end do
+     !$OMP END PARALLEL DO
+  end do
+
+end subroutine HCont
+
