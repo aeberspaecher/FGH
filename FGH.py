10300b9ce9ca274b2a2d82ec46890a8f48ca27ff
Alexander Eberspächer <alex.eberspaecher@gmail.com>
2012-10-02 09:55:57 +0200

    Add double well potential.
    Also shift and rescale wave functions in plotting.

--- FGH.py
+++ FGH.py
@@ -7,27 +7,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 from Hamiltonian import h as H, hcont as HCont  # TODO: fix lowercase name
 from scipy.linalg import eigh
+from scipy.integrate import simps
 
-L = 50  # length of x-interval to use
-N = 2**12  # number of samples
+L = 15  # length of x-interval to use
+N = 2**10  # number of samples
 NplotMin = 0  # quantum number of first eigenfunction to plot
-Nplot = 5  # number of eigenfunctions to plot
+Nplot = 7  # number of eigenfunctions to plot
 
 # define a potential:
-pot = lambda x: 0.5 * 3.0**2 * (x-L/2.)**2  # harmonic oscillator potential, centered
+pot = lambda x: +0.5 * 3.0**2 * (x-L/2.)**2  # harmonic oscillator potential, centered
+#pot = lambda x: -0.5 * 10.0**2 * (x-L/2.)**2 + (x-L/2.)**4  # double well potential
 
 # define a grid:
 x = np.linspace(0, L, N)
 
 # sample potential:
 Vsampled = pot(x)
-Hsampled = HCont(L, Vsampled)
+Hsampled = H(L, Vsampled)
 
 # diagonalize the Hamiltonian matrix:
 E, psi = eigh(Hsampled)
 
+WFscaleFactor = (np.max(Vsampled) - np.min(Vsampled))/Nplot
+plt.plot(x, Vsampled, ls="-", c="k", lw=2)
 for i in range(NplotMin, NplotMin+Nplot):
-    plt.plot(x, np.abs(psi[:,i])**2, ls="--", lw=2)
+    # physically normalize WF
+    WFnorm = np.sqrt(simps(np.abs(psi[:,i])**2, x=x))
+    psi[:,i] /= WFnorm
+    plt.plot(x, WFscaleFactor*np.abs(psi[:,i])**2 + E[i], ls="-", lw=1)
     print("E[%s] = %s"%(i, E[i]))
 
 plt.show()
